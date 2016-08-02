92122927a33ea0d1ceff8e76291aa09f28f99f96
kgraff98 <kevinjgraff98@gmail.com>
2016-08-02 11:12:01 -0500

    Created a sign in link

--- vgr/main.py
+++ vgr/main.py
@@ -18,13 +18,24 @@ import webapp2
 import jinja2
 import json
 import os
+from google.appengine.api import users
 
 env = jinja2.Environment(loader=jinja2.FileSystemLoader('templates'))
 
 class MainHandler(webapp2.RequestHandler):
-    def get(self):
-        template = env.get_template('vgr.html')
-        self.response.out.write(template.render())
+	def get(self):
+		template = env.get_template('vgr.html')
+		user = users.get_current_user()
+		if user:
+			greeting = ('Welcome, %s! (<a href="%s">sign out</a>)' %
+				(user.nickname(), users.create_logout_url('/')))
+		else:
+			greeting = ('<a href="%s"> Sign in or register</a>.' %
+				users.create_login_url('/'))
+
+		user_data = {'user' : greeting}
+		#self.response.out.write('<html><body>%s</body></html>' % greeting)
+		self.response.out.write(template.render(user_data))
 
 class ResultsHandler(webapp2.RequestHandler):
 	def get(self):
@@ -46,10 +57,11 @@ class Page4Handler(webapp2.RequestHandler):
 		template = env.get_template('page4.html')
 		self.response.write(template.render())
 
+
 app = webapp2.WSGIApplication([
-    ('/', MainHandler),
-    ('/page2', Page2Handler),
-    ('/page3', Page3Handler),
-    ('/page4', Page4Handler),
-    ('/results', ResultsHandler)
+	('/', MainHandler),
+	('/page2', Page2Handler),
+	('/page3', Page3Handler),
+	('/page4', Page4Handler),
+	('/results', ResultsHandler)
 ], debug=True)
--- vgr/static/js/vgr.js
+++ vgr/static/js/vgr.js
@@ -1,10 +1,10 @@
-  // $.getJSON('http://en.wikipedia.org/w/api.php?action=parse&page=google&prop=text&format=json&callback=?', function(json) { 
-  //   $("#information").html(json.parse(text['*'])); 
-  //   $("#information").find("a:not(.references a)").attr("href", function(){ return "http://www.wikipedia.org" + $(this).attr("href");}); 
-  //   $("#information").find("a").attr("target", "_blank"); 
-  // });
+  $.getJSON('http://en.wikipedia.org/w/api.php?action=parse&page=google&prop=text&format=json&callback=?', function(json) { 
+    $("#information").html(json.parse(text['*'])); 
+    $("#information").find("a:not(.references a)").attr("href", function(){ return "http://www.wikipedia.org" + $(this).attr("href");}); 
+    $("#information").find("a").attr("target", "_blank"); 
+  });
 
-  // 
+  
 
 //   $("#navbar > ul > li").click(function(){
 //     $("#navbar > ul > li").removeClass("active");
@@ -20,4 +20,4 @@
 	$('#submit').click(something)
   }
 
-$(document).ready(setup)
+  $(document).ready(setup)
