acf901ece0b10ab77f1b46b0ff4172df9fdafc28
kgraff98 <kevinjgraff98@gmail.com>
2016-08-02 16:04:45 -0500

    indent?

--- vgr/static/js/vgr.js
+++ vgr/static/js/vgr.js
@@ -1,23 +1,11 @@
-
-
-  $(.getJSON('https://en.wikipedia.org/w/api.php?action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=json', 
+  $(.getJSON('https://en.wikipedia.org/w/api.php?action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=json&callback=?', 
     function(json) { 
     $("#information").html(json.parse(text['*'])); 
     $("#information").find("a:not(.references a)").attr("href",
     function(){ return "http://www.wikipedia.org" + $(this).attr("href");}); 
-    $("#information").find("a").attr("target", "_blank"); }); )
+    $("#information").find("a").attr("target", "_blank"); });
  
  //$(.getJSON("http://en.wikipedia.org/w/api.php?action=parse&format=json&callback=?", {page:pageName, prop:"text"}, wikipediaHTMLResult);)
-
-
-
-
-  
-
-//   $("#navbar > ul > li").click(function(){
-//     $("#navbar > ul > li").removeClass("active");
-//     $(this).addClass('active');
-// });
 
   function something(){
 	window.location.replace('/results')
