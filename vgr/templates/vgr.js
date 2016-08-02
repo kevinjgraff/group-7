bc60d066c40cb2cb13af10020195efc21712fbb6
kgraff98 <kevinjgraff98@gmail.com>
2016-08-01 22:49:51 -0500

    commented out js file

--- vgr/templates/vgr.js
+++ vgr/templates/vgr.js
@@ -1,4 +1,7 @@
-  $.getJSON('http://en.wikipedia.org/w/api.php?action=parse&page=google&prop=text&format=json&callback=?', function(json) { 
-    $("#information").html(json.parse(text['*'])); 
-    $("#information").find("a:not(.references a)").attr("href", function(){ return "http://www.wikipedia.org" + $(this).attr("href");}); 
-    $("#information").find("a").attr("target", "_blank"); 
+  // $.getJSON('http://en.wikipedia.org/w/api.php?action=parse&page=google&prop=text&format=json&callback=?', function(json) { 
+  //   $("#information").html(json.parse(text['*'])); 
+  //   $("#information").find("a:not(.references a)").attr("href", function(){ return "http://www.wikipedia.org" + $(this).attr("href");}); 
+  //   $("#information").find("a").attr("target", "_blank"); 
+  // });
+
+  // 
